f5532c901d60cbcb3f05b47bce91f3840df44814
eric_dias <ericdiasblu@gmail.com>
2025-03-19 17:20:15 -0300

    arquivo auth_service.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:trivia_world/screens/enter_screen.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
   runApp(const MaterialApp(
     home: EnterScreen(),
     debugShowCheckedModeBanner: false,
